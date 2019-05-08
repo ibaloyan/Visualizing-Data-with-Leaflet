c00987485b238b51d5fc2049dc95e009602bf3b9
ibaloyan <ibaloyan@gmail.com>
2019-05-08 01:04:20 -0400

    Changed access token for mapbox

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+// const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+
+const API_KEY = "pk.eyJ1IjoiaWJhbG95YW4iLCJhIjoiY2psZWNuOXhmMDUyaDN2cWh2azFwOGV4diJ9.-9t1DojKUtkD0vkrDC_U0Q";
